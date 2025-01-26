7b62177cd82ef51d1d9ef4527378e55d9f382a50
David Ericson <davidestudofcg@gmail.com>
2025-01-26 18:16:28 -0300

    refactor: change font and update text color to a softer tone on the ExperienceCategory component

--- src/components/ExperienceCategory.jsx
+++ src/components/ExperienceCategory.jsx
@@ -6,38 +6,55 @@ import WorkIcon from "@mui/icons-material/Work";
 import SchoolIcon from "@mui/icons-material/School";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 
-function ExperienceCategory({ category }) {
-    const categoryIcons = {
-      Education: <SchoolIcon />,
-      Travel: <CardTravelIcon />,
-      Work: <WorkIcon />,
-      Default: <HelpOutlineIcon />,
-    };
-  
-    const icon = categoryIcons[category] || categoryIcons.Default;
-  
-    return (
-      <Stack direction="column" alignItems="center">
-        <Avatar
-          sx={{
-            bgcolor: "#FF5722",
-            transition: "transform 0.3s ease",
-            marginBottom: "7px",
-            ":hover": {
-              transform: "scale(1.2)",
-            },
-          }}
-        >
-          {icon}
-        </Avatar>
+const poppinsFont = new FontFace(
+  "Poppins",
+  "url(https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap)"
+);
 
-        <p style={{ margin: 0 }}>{category}</p>
-      </Stack>
-    );
-  }
-  
-  ExperienceCategory.propTypes = {
-    category: PropTypes.string.isRequired,
+poppinsFont.load().then(() => {
+  document.fonts.add(poppinsFont);
+});
+
+function ExperienceCategory({ category }) {
+  const categoryIcons = {
+    Education: <SchoolIcon />,
+    Travel: <CardTravelIcon />,
+    Work: <WorkIcon />,
+    Default: <HelpOutlineIcon />,
   };
-  
-  export default ExperienceCategory;  
+
+  const icon = categoryIcons[category] || categoryIcons.Default;
+
+  return (
+    <Stack direction="column" alignItems="center">
+      <Avatar
+        sx={{
+          bgcolor: "#FF5722",
+          transition: "transform 0.3s ease",
+          marginBottom: "7px",
+          ":hover": {
+            transform: "scale(1.2)",
+          },
+        }}
+      >
+        {icon}
+      </Avatar>
+
+      <p
+        style={{
+          margin: 0,
+          fontFamily: "'Poppins', sans-serif",
+          color:"rgb(86, 86, 86)",
+        }}
+      >
+        {category}
+      </p>
+    </Stack>
+  );
+}
+
+ExperienceCategory.propTypes = {
+  category: PropTypes.string.isRequired,
+};
+
+export default ExperienceCategory;
