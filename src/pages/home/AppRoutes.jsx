3d92620b486433a92d77b584e6323aa5b94c2baa
David Ericson <davidestudofcg@gmail.com>
2025-01-29 11:59:05 -0300

    feat: improve route path logic for category
    Set category list as the index route, making CategoryListPage load by default at /category-list, while specific categories remain accessible via /category-list/:category

--- src/pages/home/AppRoutes.jsx
+++ src/pages/home/AppRoutes.jsx
@@ -1,13 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import CategoriaPage from "./CategoriaPage";
 import Home from "./index";
+import CategoryPage from "./CategoryPage";
+import CategoryListPage from "./CategoryListPage";
 
 function AppRoutes() {
     return (
         <Router>
             <Routes>
+                {/* Página inicial */}
                 <Route path="/" element={<Home />} />
-                <Route path="/PaginaInicial/:category" element={<CategoriaPage />} />
+
+                <Route path="/category-list">
+                    {/* Categorias */}
+                    <Route index element={<CategoryListPage />} />
+                    <Route path=":category" element={<CategoryPage />} />
+                </Route>
+
             </Routes>
         </Router>
     );
