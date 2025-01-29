310a9e37bc9d4224c0da79f8a11d80a8986a69ad
David Ericson <davidestudofcg@gmail.com>
2025-01-29 17:17:09 -0300

    feat: implemented dynamic element generation by map

---
+++ src/pages/home/ExperienceData.js
@@ -0,0 +1,103 @@
+const experienceData = {
+    "Workshops e Aulas": [
+      {
+        id: 1,
+        imageUrl: "https://via.placeholder.com/345x140",
+        date: "12 Mar 2025",
+        title: "Workshop de Culinária",
+        location: "São Paulo, SP",
+        details: "Aprenda a cozinhar pratos incríveis com chefs renomados.",
+      },
+      {
+        id: 10,
+        imageUrl: "https://via.placeholder.com/345x140",
+        date: "12 Mar 2025",
+        title: "Workshop de Culinária",
+        location: "São Paulo, SP",
+        details: "Aprenda a cozinhar pratos incríveis com chefs renomados.",
+      },
+    ],
+    "Shows e Entretenimento": [
+      {
+        id: 2,
+        imageUrl: "https://via.placeholder.com/345x140",
+        date: "20 Abr 2025",
+        title: "Show de Comédia Stand-up",
+        location: "Rio de Janeiro, RJ",
+        details: "Uma noite de risadas com os melhores comediantes.",
+      },
+    ],
+    "Viagens e Turismo": [
+      {
+        id: 3,
+        imageUrl: "https://via.placeholder.com/345x140",
+        date: "15 Mai 2025",
+        title: "Tour Gastronômico",
+        location: "Florianópolis, SC",
+        details: "Explore os melhores restaurantes da cidade.",
+      },
+    ],
+    "Aventura e Adrenalina": [
+      {
+        id: 4,
+        imageUrl: "https://via.placeholder.com/345x140",
+        date: "10 Jun 2025",
+        title: "Aventura de Rafting",
+        location: "Foz do Iguaçu, PR",
+        details: "Uma experiência única em rafting nas cataratas.",
+      },
+    ],
+    "Experiencia em Destaque": [
+        {
+          id: 10,
+          imageUrl: "https://via.placeholder.com/345x140",
+          date: "1 Jul 2025",
+          title: "Retiro de Yoga",
+          location: "Paraty, RJ",
+          details: "Uma experiência de relaxamento profunda e meditação.",
+        },
+    ],
+    "Relaxamento e Bem-Estar": [
+      {
+        id: 5,
+        imageUrl: "https://via.placeholder.com/345x140",
+        date: "1 Jul 2025",
+        title: "Retiro de Yoga",
+        location: "Paraty, RJ",
+        details: "Uma experiência de relaxamento profunda e meditação.",
+      },
+    ],
+    "Gastronomia e Degustações": [
+      {
+        id: 6,
+        imageUrl: "https://via.placeholder.com/345x140",
+        date: "5 Ago 2025",
+        title: "Degustação de Vinhos",
+        location: "Bento Gonçalves, RS",
+        details: "Explore vinhos locais e aprenda sobre harmonização.",
+      },
+    ],
+    "Infantil e Familiar": [
+      {
+        id: 7,
+        imageUrl: "https://via.placeholder.com/345x140",
+        date: "25 Set 2025",
+        title: "Passeio ao Zoológico",
+        location: "Curitiba, PR",
+        details: "Diversão para toda a família com visita guiada ao zoológico.",
+      },
+    ],
+    "Experiências Personalizadas": [
+      {
+        id: 8,
+        imageUrl: "https://via.placeholder.com/345x140",
+        date: "30 Out 2025",
+        title: "Experiência Personalizada de Aventura",
+        location: "Pantanal, MS",
+        details: "Um pacote exclusivo para explorar o Pantanal com guia local.",
+      },
+    ],
+  };
+  
+  export default experienceData;
+  
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,52 +1,81 @@
 import ExperienceCategory from "../../components/ExperienceCategory.jsx";
+import ExperienceCard from "../../components/ExperienceCard.jsx";
+import LargeExperienceCard from "../../components/LargeExperienceCard.jsx";
 import { Box } from "@mui/material";
+import experienceData from "./ExperienceData.js";
 
 function Home() {
-    return (
-        <Box
-            sx={{
-                fontFamily: "'Poppins', sans-serif",
-                display: "flex",
-                flexDirection: "column",
-                margin: "0 auto",
-                maxWidth: "1200px",
-                width: "100%",
-                gap: "30px",
-                "& h1": {
-                    marginBottom: "16px",
-                },
-            }}
+  return (
+    <Box
+      sx={{
+        fontFamily: "'Poppins', sans-serif",
+        fontSize: "14px",
+        display: "flex",
+        flexDirection: "column",
+        margin: "0 auto",
+        maxWidth: "1400px",
+        width: "100%",
+        gap: "30px",
+        "& h1": {
+          marginBottom: "16px",
+        },
+      }}
+    >
+      <div>
+        <h1>Categorias</h1>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}
         >
-            <div>
-                <h1>Categorias</h1>
-                <div
-                    style={{
-                        display: "flex",
-                        flexWrap: "wrap",
-                        alignItems: "center",
-                        justifyContent: "space-around",
-                    }}
-                >
-                    <ExperienceCategory category="Workshops e Aulas" />
-                    <ExperienceCategory category="Shows e Entretenimento" />
-                    <ExperienceCategory category="Viagens e Turismo" />
-                    <ExperienceCategory category="Aventura e Adrenalina" />
-                    <ExperienceCategory category="Relaxamento e Bem-Estar" />
-                    <ExperienceCategory category="Gastronomia e Degustações" />
-                    <ExperienceCategory category="Infantil e Familiar" />
-                    <ExperienceCategory category="Experiências Personalizadas" />
-                </div>
-            </div>
-            <div><h1>Workshops e Aulas</h1></div>
-            <div><h1>Shows e Entretenimento</h1></div>
-            <div><h1>Viagens e Turismo</h1></div>
-            <div><h1>Aventura e Adrenalina</h1></div>
-            <div><h1>Relaxamento e Bem-Estar</h1></div>
-            <div><h1>Gastronomia e Degustações</h1></div>
-            <div><h1>Infantil e Familiar</h1></div>
-            <div><h1>Experiências Personalizadas</h1></div>
-        </Box>
-    );
+          <ExperienceCategory category="Workshops e Aulas" />
+          <ExperienceCategory category="Shows e Entretenimento" />
+          <ExperienceCategory category="Viagens e Turismo" />
+          <ExperienceCategory category="Aventura e Adrenalina" />
+          <ExperienceCategory category="Relaxamento e Bem-Estar" />
+          <ExperienceCategory category="Gastronomia e Degustações" />
+          <ExperienceCategory category="Infantil e Familiar" />
+          <ExperienceCategory category="Experiências Personalizadas" />
+        </div>
+      </div>
+
+      {Object.keys(experienceData).map((category) => (
+        <div key={category}>
+          <h1>{category}</h1>
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              gap: "20px",
+              justifyContent: "flex-start",
+            }}
+          >
+            {experienceData[category].map((experience) => (
+              category !== "Experiencia em Destaque" ? (
+                <ExperienceCard
+                key={experience.id}
+                id={experience.id}
+                imageUrl={experience.imageUrl}
+                date={experience.date}
+                title={experience.title}
+                location={experience.location}
+                details={experience.details}
+              />
+              ) : (
+                <LargeExperienceCard
+                key={experience.id}
+                event={experience}
+                />
+              )
+            ))}
+          </div>
+        </div>
+      ))}
+    </Box>
+  );
 }
 
 export default Home;
