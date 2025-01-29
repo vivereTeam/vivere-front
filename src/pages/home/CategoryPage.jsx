47b392baff29ca480ef21a5df60eb1a6e8298045
David Ericson <davidestudofcg@gmail.com>
2025-01-29 11:56:49 -0300

    feat: rename CategoriaPage to CategoryPage

--- src/pages/home/CategoryPage.jsx
+++ src/pages/home/CategoryPage.jsx
@@ -2,10 +2,12 @@ import { useParams } from "react-router-dom";
 
 function CategoriaPage() {
     const { category } = useParams();
+    const formattedCategory = decodeURIComponent(category).replace(/-/g, " ");
+
     return (
         <div style={{ padding: "20px" }}>
             <h1>Detalhes da Categoria</h1>
-            <p>Você está visualizando: {category}</p>
+            <p>Você está visualizando: {formattedCategory}</p>
         </div>
     );
 }
