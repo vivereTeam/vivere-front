e2ad567fc5d66592f7b8c491dce1e2854bb4704c
J0aoPaulo <contato.joaopaulodeveloper@gmail.com>
2025-01-30 10:50:29 -0300

    resolve

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,5 @@
-import ExperienceCard from "./../../components/ExperienceCard.jsx"
-import ExperienceCategory from "./../../components/ExperienceCategory.jsx"
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
-
 
 function Home() {
     return (
