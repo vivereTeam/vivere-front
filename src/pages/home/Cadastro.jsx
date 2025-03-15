b4d6caae15998e9edbb954b44c9dc65ca8b8f48a
Guilherme <guiilhermemacielcarneiro@gmail.com>
2025-03-15 19:23:42 -0300

    feat: add function login && cadastro

---
+++ src/pages/home/Cadastro.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/Header"
+import Footer from "../../components/Footer"
+function Cadastro() {
+    return(
+        <Header/>
+
+
+        <Footer/>
+    )
+}
+export default Cadastro;
---
+++ src/pages/home/Login.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+function Login() {
+    return(
+        <Header/>
+
+        <Footer/>
+    )
+}
+export default Login;
